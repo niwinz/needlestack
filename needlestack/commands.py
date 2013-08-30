cdc5b62d2e55e827e010c85d92a0763456fa80ec
Andrey Antukh <niwi@niwi.be>
2013-08-30 12:24:30 +0200

    Add missing code for drop_indexes command.

--- needlestack/commands.py
+++ needlestack/commands.py
@@ -6,13 +6,18 @@ import sys
 
 from needlestack.base import _get_all_indexes, _load_all_indexes
 from needlestack.connection import manager
-from needlestack.exceptions import IndexAlreadyExists
+from needlestack.exceptions import (IndexAlreadyExists,
+                                    IndexDoesNotExists)
 
 _min_verbosity_level = 1
 
 
 def sync_indexes(backend="default", verbosity=0):
-   if verbosity >= _min_verbosity_level:
+    """
+    Synchronize all registred indexes to backend.
+    """
+
+    if verbosity >= _min_verbosity_level:
         print("Syncronizing indexes...", file=sys.stderr)
 
     _load_all_indexes()
@@ -30,6 +35,23 @@ def sync_indexes(backend="default", verbosity=0):
 
 
 def drop_indexes(backend="default", verbosity=0):
-   if verbosity >= _min_verbosity_level:
+    """
+    Delete all registred indexes from backend.
+    """
+
+    if verbosity >= _min_verbosity_level:
         print("Droping all registred indexes from current backend...", file=sys.stderr)
 
+    connection = manager.get_connection(backend)
+
+    _load_all_indexes()
+    indexes = _get_all_indexes()
+
+    for index in indexes:
+        try:
+            connection.drop_index(index)
+            if verbosity > 0:
+                print("Deleting index '{0}'".format(index.get_name()), file=sys.stderr)
+        except IndexDoesNotExists:
+            if verbosity > 0:
+                print("Index '{0}' does not exist.".format(index.get_name()), file=sys.stderr)
