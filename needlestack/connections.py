d67257dfe124d74d40d1dbe8bf881df27a07bf2c
Andrey Antukh <niwi@niwi.be>
2013-09-05 12:12:08 +0200

    Add helpers for obtain indexes to connection manager.

--- needlestack/connections.py
+++ needlestack/connections.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 from . import utils
+from . import base
 
 
 class ConnectionManager(object):
@@ -38,6 +39,14 @@ class ConnectionManager(object):
 
         setattr(self._connections, alias, instance)
         return instance
+
+    def get_all_indexes(self):
+        base._load_all_indexes()
+        return base._get_all_indexes()
+
+    def get_index_by_name(self, name):
+        base._load_all_indexes()
+        return base._get_index_by_name(name)
 
     def __getattr__(self, alias):
         return self.get_connection(alias)
