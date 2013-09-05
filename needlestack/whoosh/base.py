82d984606d540bec3cde59705f96a90d63f43d32
Andrey Antukh <niwi@niwi.be>
2013-09-05 15:36:54 +0200

    Add workaround to whoosh delete index method lack.

--- needlestack/whoosh/base.py
+++ needlestack/whoosh/base.py
@@ -44,8 +44,9 @@ class Whoosh(base.SearchBackend):
         return self._storage.create_index(indexname=index.get_name(),
                                           schema=index.get_schema())
     def delete_index(self, index):
-        raise NotImplemetedError("whoosh does not have support for "
-                                 "delete individual indexes")
+        # use create_index with overwrite=True for clear index.
+        # whoos does not has support for delete index.
+        self.create_index(index, overwrite=True)
 
     def delete_all_indexes(self):
         self._storage.clean()
