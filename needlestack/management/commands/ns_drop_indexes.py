ca2269c5ae568cd63253af7bc614a79d26f7f8ac
Andrey Antukh <niwi@niwi.be>
2013-09-16 09:07:44 +0200

    Add ns_drop_indexes command.

---
+++ needlestack/management/commands/ns_drop_indexes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, unicode_literals
+
+from django.core.management.base import BaseCommand, CommandError
+from needlestack import commands
+
+
+class Command(BaseCommand):
+    help = 'Sync all defined indexes with a current backend'
+    option_list = BaseCommand.option_list + (
+                        make_option('--backend',
+                                    action='store',
+                                    dest='backend',
+                                    default='default'),)
+
+    def handle(self, *args, **options):
+        commands.drop_indexes(options["backend"], options["verbosity"])
+
+
